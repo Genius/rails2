16c841b9d15cfd915d14fa9599a2d5655162c3f9
hfsaito <hiroshi.fs94@gmail.com>
2025-03-05 21:42:28 -0300

    enhance: sending the string to script

--- .github/workflows/scripts/mesages.js
+++ .github/workflows/scripts/mesages.js
@@ -1,17 +1,17 @@
-module.exports = ({steps}) => {
+module.exports = ({prLink, diff}) => {
   const outputs = {};
 
   outputs.text = [
     "New commits were merged in rails-lts!",
     "Please review and deploy!",
-    steps.pr.outputs.result.html_url
+    prLink
   ].join('\n');
 
   outputs.markdown = [
     "**New commits were merged in rails-lts!**",
-    `[Please review and deploy!](${steps.pr.outputs.result.html_url})`,
+    `[Please review and deploy!](${prLink})`,
     "```",
-    steps['git-diff'].outputs.pretty_diff,
+    diff,
     "```"
   ].join('\n');
 
